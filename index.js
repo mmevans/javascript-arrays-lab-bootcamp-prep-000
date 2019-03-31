88dbe3fb1abba25d695a3ab546a917f5b57a7ae2
Michael Evans <mmevansjr@gmail.com>
2019-03-31 13:09:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 function destructivelyAppendKitten(name) {
-  name.push();
-  return name;
+  kittens.push(name);
+  return kittens;
 }
