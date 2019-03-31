7396892ac4272b945a6b0b8c579799b81a540e6f
Michael Evans <mmevansjr@gmail.com>
2019-03-31 13:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
